b6d048ba312dc4d64c907b7d65941c76385e80fb
James Hicks <james@motorcyclesarejets.com>
2015-01-14 15:58:43 -0600

    a basic setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='basketballdatabase',
+    version='0.5',
+    packages=['basketballdatabase'],
+    requires=['bs4', 'pandas', 'requests']
+)
